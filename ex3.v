eaa1517e6eaa75c82778752b767f284c3c6b4e1a
sakabar <takafumi.sakakibara@gmail.com>
2014-06-11 15:10:23 +0900

    New: solving task 13, 14

--- ex3.v
+++ ex3.v
@@ -53,14 +53,98 @@ Fixpoint sum (xs: list nat) : nat :=
 Theorem Pigeon_Hole_Principle :
   forall (xs : list nat), length xs < sum xs -> (exists x, 1<x /\ In x xs).
 Proof.
-  induction xs.
-  intros.
+  induction xs.  
+  intros H.
   inversion H.
 
   intros.
-  simpl.
+  exists a.
+
+  split.
+  simpl in H.
+
+
 
 Abort.
 (* Qed. *)
 
 End Task12.
+
+Module Task13.
+(* コンストラクタの名前は例えば、SOとSにするとよい。 *)
+Inductive pos : Set :=
+  (* posの定義を書く *)
+  | SO : pos
+  | S : pos -> pos.
+
+Fixpoint plus(n m:pos) : pos := (* plusの定義を書く *)
+  match n with
+  | SO => m
+  | S n' => S (plus n' m)
+  end.
+
+Infix "+" := plus.
+
+Theorem plus_assoc : forall n m p, n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p.
+  generalize n.
+  clear n.
+  induction n.
+  simpl.
+  reflexivity.
+
+  simpl.
+  rewrite IHn.
+  reflexivity.
+Qed.
+
+End Task13.
+
+Module Task14.
+
+Theorem FF: ~exists f, forall n, f (f n) = S n.
+Proof.
+  unfold not.
+  intros H.
+
+
+Abort.
+(* Qed. *)
+
+End Task14.
+
+Module Task15.
+
+Inductive Tree:Set :=
+  | Node: list Tree -> Tree.
+
+Theorem Tree_dec: forall a b:Tree, {a=b} + {a<>b}.
+Proof.
+  induction a; induction b.
+  induction l; induction l0.
+  left.
+  reflexivity.
+
+  right.
+  unfold not.
+  intro H.
+  discriminate H.
+
+  right.
+  unfold not.
+  intro H.
+  discriminate H.
+
+  rename a0 into b.
+  rename l into tr0.
+  rename l0 into tr1.
+  inversion IHl.
+  right.
+  unfold not.
+  intro H1.
+  rewrite <- H in H1.
+Abort.
+(* Qed. *)
+
+End Task15.
